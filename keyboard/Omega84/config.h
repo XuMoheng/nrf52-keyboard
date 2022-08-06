65ec74a5db7cb57ee6011a87cfa0fa0f8e6e537e
白鹭归庭 <3169232093@qq.com>
2022-08-06 16:19:43 +0800

    Update config.h

--- keyboard/Omega84/config.h
+++ keyboard/Omega84/config.h
@@ -1,7 +1,7 @@
 /*
 Copyright 2012 Jun Wako <wakojun@gmail.com>
 Copyright 2019 Jim Jiang <jim@lotlab.org>
-
+ 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
